84b257dfd0d166211397ec2b228b0ab63219a98c
Reticencia <ezequielangel141@gmail.com>
2025-06-03 21:19:29 -0700

    Creacion de la clase Biblioteca

--- src/Biblioteca.php
+++ src/Biblioteca.php
@@ -1,3 +1,21 @@
 <?php
+class Biblioteca{
+    public function __construct(
+        public $Libros = []
+    ){}
+    
+    public function agregarLibro($libro){
+        $this->Libros[] = $libro;
+    }
 
+    public function buscarLibro(string $titulo){
+        foreach($this->Libros as $libros){
+            if($libros->titulo === $titulo){
+                return $libros;
+            }
+            return null;
+        }
+    }
+
+}
 ?>
