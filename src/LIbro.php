ba08f0bc3352f2e8031b8848406d8a104633760d
Reticencia <ezequielangel141@gmail.com>
2025-06-03 18:53:55 -0700

    Creacion de todos los scrips y primer clase

--- src/LIbro.php
+++ src/LIbro.php
@@ -1,3 +1,42 @@
 <?php
 
+class Libro{
+    /*Esta es una vercion mas antigua
+    Esta no necesariamente esta mal, solo es mas manual
+
+
+
+    //Propiedades
+    public $titulo;
+    public $autor;
+    public $disponible;
+
+    //Constructor
+    public function __construct($titulo,$autor,$disponible = true){
+        $this->titulo = $titulo;
+        $this->autor = $autor;
+        $this->disponnible = $disponible;
+
+    }*/
+
+    //Esta es una vercion mucho mas moderna
+    /*
+    Esta es mas moderna, y con este crear las propiedades el constructor
+    Asigna los valores y aplica valores por defecto
+    */
+    public function __construct(
+        public string $titulo,
+        public string $autor,
+        public bool $disponible = true
+    ){}
+
+    public function prestar():void{
+        $this->disponible = false;
+    }
+
+    public function devolver():void{
+        $this->desponible = true;
+    }
+}
+
 ?>
